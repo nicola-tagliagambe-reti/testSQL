bee1e2eceedba94899df16c27a4d46aed992e57e
Nicola <>
2017-07-21 15:05:45 +0200

    Sp_test adding

--- QueryTest.sql
+++ QueryTest.sql
@@ -1,5 +1,12 @@
 -- Test file for Git Integration
 -- Date: 21/07/2017
 
-select 1
-from dual;
+create procedure Sp_Test( 
+                         pResult out number, 
+                         pResultDescr out varchar2
+                        )
+is
+begin 
+    -- First Version of Sp_Test    
+    null;
+end Sp_Test;
